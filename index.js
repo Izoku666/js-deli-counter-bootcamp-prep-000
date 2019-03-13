04057f0525244e2fae38ef7d9f6c14e93dac9020
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-13 23:18:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function takeANumber(line , name)
 {
     line.length++;
-    line[line.length - 1] = name;
-    return `Welcome, ${name}. You are number ${line.length - 1} in line.`; 
+    //line[line.length - 1] = name;
+    return `Welcome, ${name}. You are number ${line.length} in line.`; 
 }
 
